5cea8655eac8194d910957b72b1a7d0c4dc38c58
Rheena Mary Piamonte <piamonte.rheenamary@gmail.com>
2026-02-12 23:42:39 +0800

    Add user input for name and age with greeting

---
+++ LabActvityPiamonte.py
@@ -0,0 +1,7 @@
+# declare variables and get user input
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# print greeting message
+print("Hello! My name is ", name + "!")
+print("I am ", age, "years old.")
